157911009fb8a142a370704dffd6943a25de6002
Truong Do <truongdq54@gmail.com>
2017-01-21 21:46:08 +0900

    Update path

--- example/path.sh
+++ example/path.sh
@@ -1,5 +1,5 @@
 htk=../tools/htk/install
 sptk=../tools/SPTK-3.9/install
 textana=../tools/textana/
-scripts=/home/truong-d/workspace/vais/hts_sample/src/scripts
+scripts=../src/scripts
 export PATH=$htk/bin:$sptk/bin:${textana}:$PATH
